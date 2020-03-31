6fa58337c99d68753fc0d2f518d86e107fb751df
Bohomazov Dmitriy <bohomazov.dmitriy@gmail.com>
2020-03-31 17:11:28 +0300

    Updates

---
+++ precache-manifest.4bd922950c8752cefbd7a2c9db43ae3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a0cf5ab7f42d873307fe2135350e3d",
+    "url": "/weatherForecast/index.html"
+  },
+  {
+    "revision": "0bd0ebb09447f0db6fb4",
+    "url": "/weatherForecast/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0b582b7cba1e5667f7a2",
+    "url": "/weatherForecast/static/css/main.ad27dff0.chunk.css"
+  },
+  {
+    "revision": "0bd0ebb09447f0db6fb4",
+    "url": "/weatherForecast/static/js/2.ca78937c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weatherForecast/static/js/2.ca78937c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b582b7cba1e5667f7a2",
+    "url": "/weatherForecast/static/js/main.eadf2588.chunk.js"
+  },
+  {
+    "revision": "a5bc35272e66626f8e32",
+    "url": "/weatherForecast/static/js/runtime-main.524a3840.js"
+  }
+]);
